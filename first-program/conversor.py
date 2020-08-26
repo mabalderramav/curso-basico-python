fd6a7c0d654ecf5003ed11c0b4613e905a21b3b8
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2020-08-26 00:19:22 -0400

    added menu logic to conversor.py

--- first-program/conversor.py
+++ first-program/conversor.py
@@ -1,7 +1,46 @@
-pesos = input("Cuantos pesos colombianos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 3875
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("tienes $" + dolares + " dolares")
+menu = """
+Bienvenidos al conversor de monedas 💰
+
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+4 - Pesos bolivianos
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("Cuantos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3875
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("tienes $" + dolares + " dolares")
+elif opcion == 2:
+    pesos = input("Cuantos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("tienes $" + dolares + " dolares")
+elif opcion == 3:
+    pesos = input("Cuantos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("tienes $" + dolares + " dolares")
+elif opcion == 4:
+    pesos = input("Cuantos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 6.96
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("tienes $" + dolares + " dolares")
+else:
+    print('ingresa una opción correcta por favor! 😁')
